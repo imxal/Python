e328f63ad4d32e9d41ae7371cbd26e90924ff92b
laxmi <62564866+imxal@users.noreply.github.com>
2023-09-07 21:50:56 +0545

    Add files via upload

---
+++ Day 18/day18.py
@@ -0,0 +1,12 @@
+#translation->translating vowels to g
+def translate(phrase):
+    translation=""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            translation=translation + "g"
+        else:
+            translation=translation + letter
+    return translation
+
+
+print(translate(input("Enter a phrase: ")))
